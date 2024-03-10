be80ca2579d1572e4d963a678c7cd68e59043d2b
Euglena <shimidorimu.00@gmail.com>
2024-03-10 23:20:07 +0900

    ポート番号を変更しました

--- src/database/Database.ts
+++ src/database/Database.ts
@@ -1,7 +1,7 @@
 // Search-system\SearchSystemAPI\src\database\Database.ts
 
 import * as mysql from "mysql2/promise";
-import * as dotenv from 'dotenv';
+import * as dotenv from "dotenv";
 
 dotenv.config();
 
@@ -10,12 +10,13 @@ export class Database {
 
     public static async getConnection(): Promise<mysql.Connection> {
         if (!Database.connection) {
+            const port = parseInt(process.env.DB_PORT);
             Database.connection = await mysql.createConnection({
                 host: process.env.DB_HOST,
                 user: process.env.DB_USER,
                 password: process.env.DB_PASSWORD,
                 database: process.env.DB_NAME,
-                port: parseInt(process.env.DB_PORT || "3306"),
+                port: isNaN(port) ? 3306 : port,
             });
             console.log("Successfully connected to the database.");
         }
